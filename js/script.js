f495d590e6b072ef6861735d18e87a5d01ff2e2f
Fateha1 <ssyska83@gmail.com>
2023-07-16 00:06:41 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(() => {
+  $('.new-card').slice(0, 4).show()
+  $('.load-more').click(function(e){
+    e.preventDefault()
+    $('.new-card:hidden').slice(0, 4).slideDown()
+  })
+})
+  var swiper = new Swiper(".mySwiperPopuler", {
+    grabCursor: true,
+    spaceBetween:20,
+    centeredSlides: false,
+    slidesPerView: "auto",
+    navigation: {
+      nextEl: ".bxs-chevron-right-circle",
+      prevEl: ".bxs-chevron-left-circle",
+   },
+    
+    
+  });
